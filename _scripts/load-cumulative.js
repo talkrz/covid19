a99cf902790a53b9d6e5b97e8a29d9d7075be8fe
talkrz <krzysztof.talajko@gmail.com>
2020-03-04 20:55:49 +0100

    Update data

--- _scripts/load-cumulative.js
+++ _scripts/load-cumulative.js
@@ -2,6 +2,18 @@ const fetch = require('node-fetch');
 
 const url = 'https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/cases_time_v3/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Report_Date_String%20asc&resultOffset=0&resultRecordCount=2000&cacheHint=true';
 
+const dataKey = process.argv[2];
+
+const dataKeys = [
+  'Other_Locations',
+  'Mainland_China',
+];
+
+if (!dataKey || !dataKeys.includes(dataKey)) {
+  console.log('Invalid data key');
+  process.exit(-1);
+}
+
 fetch(url)
   .then(response => response.json())
   .then(json => {
@@ -9,7 +21,7 @@ fetch(url)
     json.features.forEach(dataRow => {
       result.push([
         dataRow.attributes['Report_Date_String'].replace(/\//gi, '-'),
-        dataRow.attributes['Other_Locations']
+        dataRow.attributes[dataKey]
       ]);
     })
     console.log(JSON.stringify(result, null, '  '))
--- _scripts/load.sh
+++ _scripts/load.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-node _scripts/load-cumulative-outside-china.js > src/data/cumulative-outside-china.json
-node _scripts/load-cumulative-china.js > src/data/cumulative-china.json
+node _scripts/load-cumulative.js Other_Locations > src/data/cumulative-outside-china.json
+node _scripts/load-cumulative.js Mainland_China > src/data/cumulative-china.json
