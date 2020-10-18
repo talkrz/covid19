af30afbd0a7a1a1090026fa460794d500fc15e50
talkrz <krzysztof.talajko@gmail.com>
2020-10-18 12:09:14 +0200

    Extract component

--- src/App.js
+++ src/App.js
@@ -1,76 +1,11 @@
-import React, { useState, useMemo } from 'react';
+import React from 'react';
+import HomePage from './components/screens/HomePage';
 import './App.css';
 
-import CasesCharts from './components/CasesCharts';
-import data from './data/data.json';
-import excludeCountries from './data/excludeCountries.json';
-import CountryList from './components/CountryList';
-import DateSelector from './components/DateSelector';
-import CountrySelector from './components/CountrySelector';
-import viewCharts from './dataProcessing/viewCharts';
-import viewCountryLists from './dataProcessing/viewCountryLists';
-
 function App() {
-  const [since, setSince] = useState(null)
-  const [country, setCountry] = useState('total');
-
-  const countries = useMemo(() => {
-    return Object.keys(data)
-      .filter(countryName => !excludeCountries.includes(countryName))
-      .sort();
-  }, []);
-
-  const chartsViewData = useMemo(() => viewCharts(data, country, since), [country, since]);
-  const countryLists = useMemo(() => viewCountryLists(data, excludeCountries), []);
   return (
     <div className="App">
-      <h1>Monitoring the Coronavirus disease 2019 spread pace</h1>
-
-      <div className="App-source">Source code: <a target="_blank" rel="noopener noreferrer" href="https://github.com/talkrz/2019ncov">Github</a></div>
-      <div className="App-source">COVID-19 data source: <a target="_blank" rel="noopener noreferrer"
-          href="https://github.com/CSSEGISandData/COVID-19"
-        >JHU CSSE COVID-19 Data
-        </a>
-      </div>
-
-      <div className="App-filters">
-        <DateSelector onDateSelected={setSince} />
-        <CountrySelector onCountrySelected={setCountry} country={country} countries={countries} />
-      </div>
-
-      <h2>Confirmed cases ({country})</h2>
-      <CasesCharts tableData={chartsViewData.casesAllInOne} label="Confirmed cases" />
-
-      <h2>Deaths ({country})</h2>
-      <CasesCharts tableData={chartsViewData.deathsAllInOne} label="Deaths" />
-
-      <CountryList
-        label="Countries with the biggest cases change"
-        countryList={countryLists.casesDifference}
-        formatter={v => v.toLocaleString()}
-        setCountry={setCountry}
-      />
-
-      <CountryList
-        label="Countries with the biggest growth"
-        countryList={countryLists.casesPercentage}
-        formatter={v => v.toFixed(1).toLocaleString() + '%'}
-        setCountry={setCountry}
-      />
-
-      <CountryList
-        label="Countries with the biggest deaths change"
-        countryList={countryLists.deathsDifference}
-        formatter={v => v.toLocaleString()}
-        setCountry={setCountry}
-      />
-
-      <CountryList
-        label="Countries with the biggest deaths growth"
-        countryList={countryLists.deathsPercentage}
-        formatter={v => v.toFixed(1).toLocaleString() + '%'}
-        setCountry={setCountry}
-      />
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/screens/HomePage.js
@@ -0,0 +1,76 @@
+import React, { useState, useMemo } from 'react';
+import data from '../../data/data.json';
+import excludeCountries from '../../data/excludeCountries.json';
+import viewCharts from '../../dataProcessing/viewCharts';
+import viewCountryLists from '../../dataProcessing/viewCountryLists';
+import CasesCharts from '../CasesCharts';
+import CountryList from '../CountryList';
+import DateSelector from '../DateSelector';
+import CountrySelector from '../CountrySelector';
+import './HomePage.css';
+
+export default function HomePage() {
+  const [since, setSince] = useState(null)
+  const [country, setCountry] = useState('total');
+
+  const countries = useMemo(() => {
+    return Object.keys(data)
+      .filter(countryName => !excludeCountries.includes(countryName))
+      .sort();
+  }, []);
+
+  const chartsViewData = useMemo(() => viewCharts(data, country, since), [country, since]);
+  const countryLists = useMemo(() => viewCountryLists(data, excludeCountries), []);
+
+  return (
+    <div className="HomePage">
+      <h1>Monitoring the Coronavirus disease 2019 spread pace</h1>
+
+      <div className="HomePage-source">Source code: <a target="_blank" rel="noopener noreferrer" href="https://github.com/talkrz/2019ncov">Github</a></div>
+      <div className="HomePage-source">COVID-19 data source: <a target="_blank" rel="noopener noreferrer"
+          href="https://github.com/CSSEGISandData/COVID-19"
+        >JHU CSSE COVID-19 Data
+        </a>
+      </div>
+
+      <div className="HomePage-filters">
+        <DateSelector onDateSelected={setSince} />
+        <CountrySelector onCountrySelected={setCountry} country={country} countries={countries} />
+      </div>
+
+      <h2>Confirmed cases ({country})</h2>
+      <CasesCharts tableData={chartsViewData.casesAllInOne} label="Confirmed cases" />
+
+      <h2>Deaths ({country})</h2>
+      <CasesCharts tableData={chartsViewData.deathsAllInOne} label="Deaths" />
+
+      <CountryList
+        label="Countries with the biggest cases change"
+        countryList={countryLists.casesDifference}
+        formatter={v => v.toLocaleString()}
+        setCountry={setCountry}
+      />
+
+      <CountryList
+        label="Countries with the biggest growth"
+        countryList={countryLists.casesPercentage}
+        formatter={v => v.toFixed(1).toLocaleString() + '%'}
+        setCountry={setCountry}
+      />
+
+      <CountryList
+        label="Countries with the biggest deaths change"
+        countryList={countryLists.deathsDifference}
+        formatter={v => v.toLocaleString()}
+        setCountry={setCountry}
+      />
+
+      <CountryList
+        label="Countries with the biggest deaths growth"
+        countryList={countryLists.deathsPercentage}
+        formatter={v => v.toFixed(1).toLocaleString() + '%'}
+        setCountry={setCountry}
+      />
+    </div>
+  )
+}
