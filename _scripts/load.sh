ab677a17679b2f2977e1701aceb11a50f3432193
talkrz <krzysztof.talajko@gmail.com>
2021-06-27 12:11:53 +0200

    Rebase local branch of data source repository after pulling changes

--- _scripts/load.sh
+++ _scripts/load.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd data_sources/COVID-19/ && git pull origin master && cd ../../
+cd data_sources/COVID-19/ && git pull --rebase origin master && cd ../../
 node _scripts/load.js > src/data/data.json
